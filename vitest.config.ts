b21b71d7e38da782356b8284b22b12f644b61530
Weirdo <adroszak@weirdo.codes>
2025-10-27 13:49:08 +0100

    chore(test): add alias mapping to vitest config for TS path aliases

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,15 @@
+import path from 'node:path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, 'src'),
+      '@lib': path.resolve(__dirname, 'lib'),
+      '@schema': path.resolve(__dirname, 'schema-types'),
+      '@static': path.resolve(__dirname, 'static'),
+    },
+  },
   test: {
     include: ['test/**/*.test.ts'],
     environment: 'node',
